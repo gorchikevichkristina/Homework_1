05fb5705fd1ba2c801eadda2b586ea9f036ccf4a
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-06-23 01:36:58 +0300

    задачи переписаны третий раз, создан удаленный репозиторий с таким же названием т.к я перенесла задачи в другую папку и все сломалось

--- task4_dz/Program.cs
+++ task4_dz/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите первое число: "); //Приглашение пользователя ко вводу числа (вывод в терминал)
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: "); //Приглашение пользователя ко вводу числа (вывод в терминал)
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: "); //Приглашение пользователя ко вводу числа (вывод в терминал)
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > number1) max = number2;
+if (number3 > number2) max = number3;
+
+Console.Write(max);
--- task6_dz/Program.cs
+++ task6_dz/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+///4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Write("Введите число: "); //Приглашение пользователя ко вводу числа (вывод в терминал)
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    System.Console.WriteLine("четное");
+}
+else
+{
+    System.Console.WriteLine("не чётное");
+}
